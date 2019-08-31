a0643888a91ee01b6c39e183f46ed2149a1a66ec
Captain Fantasy <captain@fantasyui.com>
2019-08-31 19:47:23 -0400

    Code Cleanup & Formatting

--- public/modules/input-component/index.js
+++ public/modules/input-component/index.js
@@ -1,21 +1,23 @@
-import uuid from '/modules/uuid/index.js';
-
 export default async function ({emitter}){
+
   class InputComponent extends HTMLElement {
+
     constructor(...args) {
+
       const self = super(...args);
       const templateSelector = '#input-component';
       const template = document.querySelector(templateSelector);
       const templateContent = template.content;
-      const shadowRoot = this.attachShadow({mode: 'open'});
+
+      this.attachShadow({mode: 'open'});
       this.shadowRoot.appendChild(templateContent.cloneNode(true));
 
       const frmMain = this.shadowRoot.querySelector('form');
-      const label = frmMain.querySelector('form label');
-      const input = frmMain.querySelector('form input');
+      const lblMain = frmMain.querySelector('form label');
+      const txtMain = frmMain.querySelector('form input');
 
-      input.value = this.getAttribute('initial');
-      label.textContent = this.getAttribute('message');
+      lblMain.textContent = this.getAttribute('message');
+      txtMain.value = this.getAttribute('initial');
 
       frmMain.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -28,10 +30,11 @@ export default async function ({emitter}){
         this.dispatchEvent(dataEvent);
       });
 
-
       return self;
     }
+
   }
+
   customElements.define('input-component', InputComponent);
   return InputComponent;
 }
